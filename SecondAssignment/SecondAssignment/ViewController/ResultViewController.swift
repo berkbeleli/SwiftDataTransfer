71dd6d814857b08a2eaf8c49595cc13b48fd49a7
Berk <berkbeleli@gmail.com>
2022-05-31 15:21:27 +0300

    Created object connections between Resultview and its controller

--- SecondAssignment/SecondAssignment/ViewController/ResultViewController.swift
+++ SecondAssignment/SecondAssignment/ViewController/ResultViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
-    override func viewDidLoad() {
+  @IBOutlet weak var totalAmountLabel: UILabel!
+  @IBOutlet weak var tipAmountLabel: UILabel!
+  @IBOutlet weak var billForEachLabel: UILabel!
+  @IBOutlet weak var reCalculateButton: UIButton!
+  
+  override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    reCalculateButton.layer.cornerRadius = 5
+    
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
