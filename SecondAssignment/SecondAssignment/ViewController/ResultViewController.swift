05cfcc0780bedc6b691a0846f431d471c374f669
Berk <berkbeleli@gmail.com>
2022-05-31 15:47:34 +0300

    Confirmed delegation for resultview
    Created reqired functions and set received values to the labels

--- SecondAssignment/SecondAssignment/ViewController/ResultViewController.swift
+++ SecondAssignment/SecondAssignment/ViewController/ResultViewController.swift
@@ -21,11 +21,30 @@ class ResultViewController: UIViewController {
     reCalculateButton.layer.cornerRadius = 5
     
     delegate?.delegate = self
+    delegate.makeCalculation() // call the function to calculate results according tto choices
   }
   
   
   @IBAction func reCalculateButtonClicked(_ sender: UIButton) {
+    self.dismiss(animated: true)
+    delegate.clearScreen() //  clear the calculationView Screen
   }
   
+  
+}
+
+extension ResultViewController: CalculationViewControllerDelegate {
+  
+  func didEnteredBill(tip: TipModel) {
+    let tipBrain = TipBrain(tip: tip)// sends the received tipModel object to our TipBrain struct so we can calculate things we want
+    totalAmountLabel.text = String(tipBrain.getBillAmount()) // get sum of the bill and sets the value of label
+    tipAmountLabel.text = String(tipBrain.getTipAmount())// get tip amount value
+    billForEachLabel.text = String(tipBrain.getBillForEachPerson()) // get bill for each person
+  }
+  
+  
+  func didFailWithError(error: Error) {
+    print(error.localizedDescription)
+  }
   
 }
