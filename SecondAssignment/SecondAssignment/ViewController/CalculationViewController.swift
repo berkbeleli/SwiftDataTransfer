26e307da25d67e29ab9ffe2539de9678dcf957f2
Berk <berkbeleli@gmail.com>
2022-05-31 13:24:46 +0300

    Created connections between CalculationView and its Controller

--- SecondAssignment/SecondAssignment/ViewController/CalculationViewController.swift
+++ SecondAssignment/SecondAssignment/ViewController/CalculationViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class CalculationViewController: UIViewController {
-
+  @IBOutlet weak var avatarImage: UIImageView!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var usernameLabel: UILabel!
+  @IBOutlet weak var billAmountTextField: UITextField!
+  @IBOutlet weak var billPercentageSegment: UISegmentedControl!
+  @IBOutlet weak var numberOfPeopleStepper: UIStepper!
+  @IBOutlet weak var numberOfPeopleLabel: UILabel!
+  @IBOutlet weak var calculateButton: UIButton!
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
