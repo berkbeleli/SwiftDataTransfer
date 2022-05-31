5735c090cbca2c13ea79a27f906808d24a611740
Berk <berkbeleli@gmail.com>
2022-05-31 12:50:30 +0300

    Added initial swift folder

---
+++ SecondAssignment/SecondAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondAssignment
+//
+//  Created by Berk Beleli on 2022-05-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
